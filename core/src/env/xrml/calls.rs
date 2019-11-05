cfea5b098cf8ec0b282897b3c37bbd864a6f8a2d
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2019-11-05 14:30:55 +0800

    Add DefaultXrmlTypes with chainx calls supported (#3)
    * Add old-codec and no-ink-alloc features (#1)
    
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>
    
    * Add DefaultXrmlTypes
    
    * Nits
    
    * Remove assets local dependency
    
    * Make Contract new command adaptive for ChainX
    
    * Update core/src/lib.rs
    
    Co-Authored-By: Qinxuan Chen <koushiro.cqx@gmail.com>
    
    * Resolove review comments and some other nits
    
    * Nits

---
+++ core/src/env/xrml/calls.rs
@@ -0,0 +1,235 @@
+// Copyright 2019 Chainpool
+//
+// Copyright 2019 Parity Technologies (UK) Ltd.
+// This file is part of ink!.
+//
+// ink! is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ink! is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ink!.  If not, see <http://www.gnu.org/licenses/>.
+
+use crate::env::EnvTypes;
+use core::convert::TryInto;
+use scale::{
+    Decode,
+    Encode,
+    Input,
+    Output,
+};
+
+#[cfg_attr(feature = "test-env", derive(Debug, Clone, PartialEq, Eq))]
+pub enum Address<T: EnvTypes, AccountIndex> {
+    Id(T::AccountId),
+    Index(AccountIndex),
+}
+
+fn need_more_than<T: PartialOrd>(a: T, b: T) -> Option<T> {
+    if a < b {
+        Some(b)
+    } else {
+        None
+    }
+}
+
+/// Decode implementation copied over from Substrate `Address` that can be found [here](substrate-address).
+///
+/// # Note
+/// This implementation MUST be kept in sync with substrate, tests below will ensure that.
+///
+/// [substrate-address]: https://github.com/paritytech/substrate/blob/ec62d24c602912f07bbc416711376d9b8e5782c5/srml/indices/src/address.rs#L61
+impl<T, AccountIndex> Decode for Address<T, AccountIndex>
+where
+    T: EnvTypes,
+    AccountIndex: Decode + From<u32> + PartialOrd + Copy + Clone,
+{
+    fn decode<I: Input>(input: &mut I) -> Option<Self> {
+        Some(match input.read_byte()? {
+            x @ 0x00..=0xef => Address::Index(AccountIndex::from(x as u32)),
+            0xfc => {
+                Address::Index(AccountIndex::from(need_more_than(
+                    0xef,
+                    u16::decode(input)? as u32,
+                )?))
+            }
+            0xfd => {
+                Address::Index(AccountIndex::from(need_more_than(
+                    0xffff,
+                    u32::decode(input)?,
+                )?))
+            }
+            0xfe => {
+                Address::Index(need_more_than(
+                    AccountIndex::from(0xffffffffu32),
+                    Decode::decode(input)?,
+                )?)
+            }
+            0xff => Address::Id(Decode::decode(input)?),
+            _ => return None,
+        })
+    }
+}
+
+/// Encode implementation copied over from Substrate `Address` that can be found [here](substrate-address).
+///
+/// # Note
+/// This implementation MUST be kept in sync with substrate, tests below will ensure that.
+///
+/// [substrate-address]: https://github.com/paritytech/substrate/blob/ec62d24c602912f07bbc416711376d9b8e5782c5/srml/indices/src/address.rs#L83
+impl<T, AccountIndex> Encode for Address<T, AccountIndex>
+where
+    T: EnvTypes,
+    AccountIndex: Encode + TryInto<u32> + Copy + Clone,
+{
+    fn encode_to<O: Output>(&self, dest: &mut O) {
+        match *self {
+            Address::Id(ref i) => {
+                dest.push_byte(255);
+                dest.push(i);
+            }
+            Address::Index(i) => {
+                let maybe_u32: Result<u32, _> = i.try_into();
+                if let Ok(x) = maybe_u32 {
+                    if x > 0xffff {
+                        dest.push_byte(253);
+                        dest.push(&x);
+                    } else if x >= 0xf0 {
+                        dest.push_byte(252);
+                        dest.push(&(x as u16));
+                    } else {
+                        dest.push_byte(x as u8);
+                    }
+                } else {
+                    dest.push_byte(254);
+                    dest.push(&i);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Encode, Decode)]
+#[cfg_attr(feature = "test-env", derive(Debug, Clone, PartialEq, Eq))]
+pub enum XAssets<T: EnvTypes, AccountIndex> {
+    #[allow(non_camel_case_types)]
+    #[codec(index = "3")]
+    transfer(
+        Address<T, AccountIndex>,
+        xassets::Token,
+        T::Balance,
+        xassets::Memo,
+    ),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        calls,
+        AccountIndex,
+        Call,
+        ChainXRuntimeTypes,
+    };
+
+    use scale::{
+        Decode,
+        Encode,
+    };
+    use srml_indices::address;
+
+    #[test]
+    fn account_index_serialization() {
+        let account_index = 0u32;
+
+        let ink_address = Address::Index(account_index.into());
+        let srml_address: address::Address<[u8; 32], u32> =
+            address::Address::Index(account_index);
+
+        let ink_encoded = ink_address.encode();
+        let srml_encoded = srml_address.encode();
+
+        assert_eq!(srml_encoded, ink_encoded);
+
+        let srml_decoded: address::Address<[u8; 32], u32> =
+            Decode::decode(&mut ink_encoded.as_slice())
+                .expect("Account Index decodes to srml Address");
+        let srml_encoded = srml_decoded.encode();
+        let ink_decoded: Address<ChainXRuntimeTypes, u32> =
+            Decode::decode(&mut srml_encoded.as_slice())
+                .expect("Account Index decodes back to ink type");
+
+        assert_eq!(ink_address, ink_decoded);
+    }
+
+    #[test]
+    fn account_id_serialization() {
+        let account_id = [0u8; 32];
+
+        let ink_address = Address::Id(account_id.into());
+        let srml_address: address::Address<[u8; 32], u32> =
+            address::Address::Id(account_id);
+
+        let ink_encoded = ink_address.encode();
+        let srml_encoded = srml_address.encode();
+
+        assert_eq!(srml_encoded, ink_encoded);
+
+        let srml_decoded: address::Address<[u8; 32], u32> =
+            Decode::decode(&mut ink_encoded.as_slice())
+                .expect("Account Id decodes to srml Address");
+        let srml_encoded = srml_decoded.encode();
+        let ink_decoded: Address<ChainXRuntimeTypes, u32> =
+            Decode::decode(&mut srml_encoded.as_slice())
+                .expect("Account Id decodes decodes back to ink type");
+
+        assert_eq!(ink_address, ink_decoded);
+    }
+
+    #[test]
+    fn call_balance_transfer() {
+        let balance = 10_000;
+        let account_index = 0;
+
+        let contract_address = calls::Address::Index(account_index);
+        let contract_transfer =
+            calls::XAssets::<ChainXRuntimeTypes, AccountIndex>::transfer(
+                contract_address,
+                b"PCX".to_vec(),
+                balance,
+                b"memo".to_vec(),
+            );
+        let contract_call = Call::XAssets(contract_transfer);
+
+        let srml_address = address::Address::Index(account_index);
+
+        let srml_transfer = xassets::Call::<chainx_runtime::Runtime>::transfer(
+            srml_address,
+            b"PCX".to_vec(),
+            balance,
+            b"memo".to_vec(),
+        );
+
+        let srml_call = chainx_runtime::Call::XAssets(srml_transfer);
+
+        let contract_call_encoded = contract_call.encode();
+        let srml_call_encoded = srml_call.encode();
+
+        assert_eq!(srml_call_encoded, contract_call_encoded);
+
+        let srml_call_decoded: chainx_runtime::Call =
+            Decode::decode(&mut contract_call_encoded.as_slice())
+                .expect("Balances transfer call decodes to srml type");
+        let srml_call_encoded = srml_call_decoded.encode();
+        let contract_call_decoded: Call =
+            Decode::decode(&mut srml_call_encoded.as_slice())
+                .expect("Balances transfer call decodes back to contract type");
+        assert_eq!(contract_call, contract_call_decoded);
+    }
+}
--- examples/lang/pcx_transfer/src/lib.rs
+++ examples/lang/pcx_transfer/src/lib.rs
@@ -1,33 +1,50 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate alloc;
+
+use alloc::vec::Vec;
+
 use ink_core::{
+    env::{
+        chainx_calls,
+        AccountIndex,
+        DefaultXrmlTypes,
+    },
     memory::format,
     storage,
 };
 use ink_lang::contract;
 
 contract! {
-    #![env = ink_core::env::DefaultXrmlTypes]
+    #![env = DefaultXrmlTypes]
 
     /// This simple dummy contract has a `bool` value that can
     /// alter between `true` and `false` using the `flip` message.
     /// Users can retrieve its current state using the `get` message.
-    struct {{camel_name}} {
+    struct PcxTransfer {
         /// The current state of our flag.
         value: storage::Value<bool>,
     }
 
-    impl Deploy for {{camel_name}} {
+    impl Deploy for PcxTransfer {
         /// Initializes our state to `false` upon deploying our smart contract.
         fn deploy(&mut self) {
             self.value.set(false)
         }
     }
 
-    impl {{camel_name}} {
+    impl PcxTransfer {
         /// Flips the current state of our smart contract.
         pub(external) fn flip(&mut self) {
             *self.value = !*self.value;
+        }
+
+        /// Dispatches a `transfer` call to the Balances srml module
+        pub(external) fn pcx_transfer(&mut self, dest: AccountId, value: Balance, memo: Vec<u8>) {
+            let dest_addr = chainx_calls::Address::Id(dest);
+            env.println(&format!("pcx_transfer dest: {:?}, value: {:?}", dest, value));
+            let transfer_call = chainx_calls::XAssets::<DefaultXrmlTypes, AccountIndex>::transfer(dest_addr, b"PCX".to_vec(), value, memo);
+            env.dispatch_call(transfer_call);
         }
 
         /// Returns the current state.
@@ -44,7 +61,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut contract = {{camel_name}}::deploy_mock();
+        let mut contract = PcxTransfer::deploy_mock();
         assert_eq!(contract.get(), false);
         contract.flip();
         assert_eq!(contract.get(), true);

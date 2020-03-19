93faacd1cd9973cf9481924aad9136e2217741dd
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-03-20 00:24:38 +0800

    Add some docs

--- examples/xbtc_silly_game/lib.rs
+++ examples/xbtc_silly_game/lib.rs
@@ -1,6 +1,47 @@
 #![feature(proc_macro_hygiene)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
+//! # Example for XBTC-based Dapp on ChainX
+//!
+//! This implements a demo for developing a DAPP using XBTC of ChainX.
+//!
+//! ## Warning
+//!
+//! This contract is an *example*. It is not complete, and no guarantee for any kind of security.
+//!
+//! ## Overview
+//!
+//! A typical flow of XBTC-based DAPP is about 3 steps:
+//!
+//! 1. User deposits some XBTC token into the contract to participate the game, i.e.,
+//!    transfer some XBTC token to the DAPP conrtact.
+//!
+//! 2. Main logic of the DAPP contract.
+//!
+//! 3. Reward the winners of the game, i.e., transfer some XBTC from the DAPP contract
+//!    to some external user account.
+//!
+//! Each instantiation of this contract has a set of `owners` and a `requirement` of
+//! how many of them need to agree on a `Transaction` for it to be able to be executed.
+//! Every owner can submit a transaction and when enough of the other owners confirm
+//! it will be able to be executed. The following invariant is enforced by the contract:
+//!
+//! ## Interface
+//!
+//! ### Deposit XBTC Token to DAPP Contract
+//!
+//! `deposit` is used for depositing some XBTC token to this DAPP contract to start the game.
+//!
+//! Before invoking `deposit` method, user must invoke `approve` method of XRC20 contract ahead,
+//! otherwise the deposit would fail.
+//!
+//! Note:
+//! The XRC20 contract address can be retrived by the RPC of ChainX node `chainx_contractXRCTokenInfo`.
+//!
+//! ### Reward user from DAPP Contract
+//!
+//! `reward` is to transfer some XBTC token from this DAPP contract to some external account.
+
 use ink_lang as ink;
 
 #[ink::contract(version = "0.1.0", env = DefaultXrmlTypes)]
@@ -34,40 +75,12 @@ mod xbtc_silly_game {
         }
 
         /// Transfer some value of xbtc token from this DAPP to some external account.
+        /// You might do not want to expose this interface publicly in the production case.
         ///
         /// Delegate the `transfer` call of xrc20 contract.
         #[ink(message)]
         fn reward(&mut self, receiver: AccountId, value: u64) -> bool {
             self.xrc20_contract.transfer(receiver, value)
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[test]
-        fn default_works() {
-            // Note that even though we defined our `#[ink(constructor)]`
-            // above as `&mut self` functions that return nothing we can call
-            // them in test code as if they were normal Rust constructors
-            // that take no `self` argument but return `Self`.
-            let pcx_transfer = PcxTransfer::default();
-            assert_eq!(pcx_transfer.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut pcx_transfer = PcxTransfer::new(false);
-            assert_eq!(pcx_transfer.get(), false);
-            pcx_transfer.flip();
-            assert_eq!(pcx_transfer.get(), true);
         }
     }
 }
